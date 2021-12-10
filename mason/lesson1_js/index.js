997b41871793d7b2f0d2af3b039f8d5eb2bdb045
Jasondecode2020 <zzjasonzzj@gmail.com>
2021-12-10 21:34:02 +1100

    1h

--- mason/lesson1_js/index.js
+++ mason/lesson1_js/index.js
@@ -23,9 +23,110 @@ console.log(fruit);
 var fruit = "apple";
 */
 
+/*
 foo();
 
 function foo() {
   var pear = "pear";
   console.log(pear); //pear
+}*/
+
+/*
+var fruit = "apple";
+if (true) {
+  // think in another file
+  var fruit = "pear";
+}
+console.log(fruit);
+*/
+
+/*
+let fruit = "apple";
+if (true) {
+  // think in another file
+  let fruit = "pear";
+}
+console.log(fruit);
+*/
+
+// var fruit = "apple";
+// var fruit = "pear";
+// console.log(fruit);
+
+// let fruit = "apple";
+// let fruit = "pear";
+// console.log(fruit); // Identifier 'fruit' has already been declared
+
+// let fruit = "apple";
+// fruit = "pear";
+// console.log(fruit); // Ok
+
+// console.log(fruit); // Cannot access 'fruit' before initialization
+// let fruit = "apple";
+
+// const fruit = "apple";
+// fruit = "grape"; // Assignment to constant variable.
+// console.log(fruit);
+
+/*
+const fruit = { name: "apple", color: "red" };
+// fruit = { name: "apple", color: "green" }; // Assignment to constant variable.
+fruit.color = "green";
+console.log(fruit);
+
+const fruits = [];
+// fruits = ["apple"]; // Assignment to constant variable.
+fruits.push("apple");
+console.log(fruits);
+*/
+
+// console.log(fruits); // fruits is not defined
+
+// quiz 1
+/*
+var i = 5;
+for (var i = 0; i < 3; i++) {
+  console.log(i);
+}
+console.log(i);
+*/
+
+// result
+// 0;
+// 1;
+// 2;
+// 3;
+
+// quiz 2
+/*
+let i = 5;
+for (let i = 0; i < 3; i++) { // here is block
+  console.log(i);
+}
+console.log(i);
+*/
+// result
+// 0;
+// 1;
+// 2;
+// 5;
+
+// quiz 3 trick
+/*
+setTimeout is callback function, first wait for 10s, then console, but at 
+this time, the i is already 5 and arr[5] is undefined
+*/
+// var arr = [10, 12, 15, 21, 1];
+// for (var i = 0; i < arr.length; i++) {
+//   setTimeout(function () {
+//     console.log("Index: " + i + ", element: " + arr[i]);
+//   }, 10000);
+// }
+
+var arr = [10, 12, 15, 21, 1];
+for (var i = 0; i < arr.length; i++) {
+  setTimeout(function () {
+    console.log("Index: " + i + ", element: " + arr[i]);
+  }, 2000);
+  console.log("Index: " + i + ", element: " + arr[i]);
 }
--- mason/readme.md
+++ mason/readme.md
@@ -1,5 +1,7 @@
 ## backend
 
+30 hours
+
 nodeJS include js
 
 ### lesson 1
@@ -69,5 +71,55 @@ foo();
 function foo() {
   var pear = "pear";
   console.log(pear); //pear
+}
+```
+
+compare why use let and not use var, var can be changed, let cannot
+
+```js
+var fruit = "apple";
+if (true) {
+  // think in another file
+  var fruit = "pear";
+}
+console.log(fruit);
+
+let fruit = "apple";
+if (true) {
+  // think in another file
+  let fruit = "pear";
+}
+console.log(fruit);
+```
+
+Hoisting for let is different
+
+```js
+console.log(fruit); // Cannot access 'fruit' before initialization
+let fruit = "apple";
+```
+
+pass by value or reference
+
+```js
+const fruit = { name: "apple", color: "red" };
+// fruit = { name: "apple", color: "green" }; // Assignment to constant variable.
+fruit.color = "green";
+console.log(fruit);
+
+const fruits = [];
+// fruits = ["apple"]; // Assignment to constant variable.
+fruits.push("apple");
+console.log(fruits);
+```
+
+// quiz 3
+
+```js
+var arr = [10, 12, 15, 21, 1];
+for (var i = 0; i < arr.length; i++) {
+  setTimeout(function () {
+    console.log("Index: " + i + ", element: " + arr[i]);
+  }, 1000);
 }
 ```
